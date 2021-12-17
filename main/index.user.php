5dd9f1f7e278b3814f3652b2e8f73ca01bed506c
Pierre <blanchardpierre03@gmail.com>
2021-12-17 10:39:38 +0100

    Début dashboard

---
+++ main/index.user.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0, width=device-width">
+
+    <title>
+        Accueil | MyKey3D
+    </title>
+
+    <link rel="stylesheet" href="assets/css/index.user.css">
+</head>
+<body>
+    <header>
+        <h2>MyKey3D</h2>
+        <div class="right">
+            <li><a href="">Se déconnecter</a></li>
+        </div>
+    </header>
+    <nav class="side-bar">
+
+    </nav>
+    <div class="inside-container">
+        <div class="tacktime">
+            <div class="tacktime-header">
+                <h1>Tack Time actuel</h1>
+            </div>
+        </div>
+        <div class="global-production">
+
+        </div>
+    </div>
+</body>
+</html>
