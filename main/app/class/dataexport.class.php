619f31d766c62e5d74b5e591bfb675ae37a7388d
PierroootEL <blanchardpierre03@gmail.com>
2022-04-21 06:33:30 +0000

    Gitingore + Data Export

--- main/app/class/dataexport.class.php
+++ main/app/class/dataexport.class.php
@@ -30,9 +30,9 @@
                 'SELECT * FROM operation'
             )->fetchAll();
 
-            $fp = fopen('/var/www/pierre/projet_E6.2/main/src/files/workbench_' . date('d-m-Y_H-i-s') . '.csv', 'w');
+            $fp = fopen('/var/www/pierre/projet_E6.2/main/src/files/operation_' . date('d-m-Y_H-i-s') . '.csv', 'w');
 
-            $entete = array('workbench_id', 'workbench_name', 'assigned_operation');
+            $entete = array('operation_id', 'product_value', 'quantity', 'create_date', 'status');
             fputcsv($fp, $entete, ';');
 
             foreach ($workbenchs as $workbench){
